ce12c1f37b6f10a9ea056d3de42d854500a7e079
omnia <omniacis1@gmail.com>
2025-02-08 17:12:33 +0200

    rename bloc folder

--- lib/presentation/view_model/notification/notification_state.dart
+++
@@ -1,7 +0,0 @@
-import '../../../data/models/notification_model.dart';
-
-class NotificationState {
-  final List<NotificationModel> notificationModel;
-
-  NotificationState({required this.notificationModel});
-}
